0bb649221dd44af6c7a8d4a4fdf7daed63d9e760
tkmct <1220t.takamichi@gmail.com>
2020-01-07 20:39:13 +0900

    update sync manager to store root hash

--- src/managers/SyncManager.ts
+++ src/managers/SyncManager.ts
@@ -14,7 +14,20 @@ export default class SyncManager {
     return Coder.decode(BigNumber.default(), d)
   }
 
-  public async updateSyncedBlockNumber(blockNumber: BigNumber): Promise<void> {
-    this.db.put(LATEST_SYNCED_BLOCK, Coder.encode(blockNumber))
+  public async getRoot(blockNumber: BigNumber): Promise<Bytes | null> {
+    return await this.db.get(Coder.encode(blockNumber))
+  }
+
+  /**
+   * update synced block number and save root hash of the block
+   * @param blockNumber block number to be set as LATEST_SYNCED_BLOCK
+   * @param root root hash of the newly synced block
+   */
+  public async updateSyncedBlockNumber(
+    blockNumber: BigNumber,
+    root: Bytes
+  ): Promise<void> {
+    await this.db.put(LATEST_SYNCED_BLOCK, Coder.encode(blockNumber))
+    await this.db.put(Coder.encode(blockNumber), root)
   }
 }
